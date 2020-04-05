9f66bbb4d9444ee02db92ac38f77b5f07323633b
ldimaggio <61212328+ldimaggio@users.noreply.github.com>
2020-04-05 16:57:38 -0700

    Update pipeline_skeleton.py

--- pipeline_skeleton.py
+++ pipeline_skeleton.py
@@ -152,25 +152,26 @@ def get_neighbors(point_x, point_y, rast, max_dist):
     # get something like a list of all neighbors,
     # expressed as tuples of (neigh_x, neigh_y, neigh_dist)
     neighs = [(), (), (), ...]
-    
-    # take all the cells within a certain radius of the raster point of radius size x
+	
+	# take all the cells within a certain radius of the raster point of radius size x
 	rast_coord = make_array(point_x, point_y)
-    rast_neighs =  st_buffer(rast_coord, dist, nQuadSegs)
+	rast_neighs =  st_buffer(rast_coord, dist, nQuadSegs)
     
     #filter out certain neighbors
     def sort_neighs(x)
-        if x = rast_neighs_water:
+		if x = rast_neighs_water:
             # filter out bodies of water
-	        return #get x and y coordinate and raster 
-        else x = rast_neighs_ag:
+			return # get x and y coordinate and raster 
+		else x = rast_neighs_ag:
             # filter out certain agricultural lands
-	        return #get x and y coordinate and raster 
+			return # get x and y coordinate and raster 
    
     # Extract list of all neighbors that make the cut
-    sorted_neighs = sort_neighs(x)
+	sorted_neighs = sort_neighs(x)
     
     #Extract the distance of all neighbors from ‘neigh_coord’ that make the cut
-    neigh_dist = st_distance(rast_coord, sorted_neighs) # Takes raster layer coordinate and neighbor coordinate 
+	neigh_dist = st_distance(rast_coord, sorted_neighs) 
+		# Takes raster layer coordinate and neighbor coordinate 
     
     neighs = append(neigh_coord, neigh_dist)
     
