e5b0c837afdf6dd5bf8b82f9e897c46cea64aaa4
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2022-03-01 00:20:27 -0500

    Added a job script for the asynch RF analysis.

---
+++ analysis/asynch_corr/ch3_rf_job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=ch3_rf
+#
+# Account:
+#SBATCH --account=fc_landgen
+#
+# Partition:
+#SBATCH --partition=savio3
+#
+# Wall clock limit:
+#SBATCH --time=72:00:00
+#
+# Email options
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=drew.hart@berkeley.edu
+#
+## Command(s) to run:
+
+module load gdal r r-packages r-spatial
+Rscript --vanilla /global/home/drewhart/seasonality/seasonal_asynchrony/analysis/asynch_corr/asynch_analysis.r > ch_rf.Rout 
+
