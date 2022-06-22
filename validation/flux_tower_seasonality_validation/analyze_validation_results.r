cdfff678de0c797dd8e181550e43fa81abfd8db4
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2022-06-21 20:13:41 -0400

    Added time series length as a variable being stored in the flux-tower validation.

--- validation/flux_tower_seasonality_validation/analyze_validation_results.r
+++ validation/flux_tower_seasonality_validation/analyze_validation_results.r
@@ -1,33 +1,35 @@
 library(plotbiomes)
 library(ggplot2)
 library(viridis)
+library(betareg)
 
 df = read.csv('./FLUXNET_validation_results.csv')
 df$map_cm = df$map/10
 df$one_minus_r2 = 1 - df$r2
+df$cell_dist = as.factor(df$cell_dist)
 
 # plot Euclidean distance between RS-fitted seasonality and
 # FLUXNET GPP seasonality on top of Whittaker biomes
 
-png('flux_val_results_biome.png',
-    width=1000, height=600)
-
-ggplot() +
-   geom_polygon(data = Whittaker_biomes, aes(x = temp_c,
-                                             y = precp_cm,
-                                             fill = biome,
-                                             alpha=-1.01),
-                colour = "gray97", # colour of polygon border
-                size   = -1.5) +    # thickness of polygon border
-   geom_point(data=df, aes(x=mat, y=map_cm, col=dist, size=dist, alpha=0.7)) + 
-   scale_color_viridis(option = "plasma")
-
-dev.off()
+#png('flux_val_results_biome.png',
+#    width=1000, height=600)
+#
+#ggplot() +
+#   geom_polygon(data = Whittaker_biomes, aes(x = temp_c,
+#                                             y = precp_cm,
+#                                             fill = biome,
+#                                             alpha=-1.01),
+#                colour = "gray97", # colour of polygon border
+#                size   = -1.5) +    # thickness of polygon border
+#   geom_point(data=df, aes(x=mat, y=map_cm, col=dist, size=dist, alpha=0.7)) + 
+#   scale_color_viridis(option = "plasma")
+#
+#dev.off()
 
 # build regression model of goodness of fit
-mod = lm(sqrt(r2) ~ lon + lat + mat + map + cell_dist, data=df)
-print(summary(mod))
+beta_mod = betareg(r2 ~ lon + lat + mat + map + cell_dist + gpp_ts_len, data=df)
+print(summary(beta_mod))
 
-mod_reduced = lm(sqrt(r2) ~ cell_dist, data=df)
-print(summary(mod_reduced))
+beta_mod_reduced = lm(r2 ~ mat + cell_dist + gpp_ts_len, data=df)
+print(summary(beta_mod_reduced))
 
