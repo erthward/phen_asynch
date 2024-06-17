eab7ca8f1192973821e7ad9df8898b7a1e90cdcc
Drew Hart <drew.ellison.hart@gmail.com>
2024-06-17 03:31:32 -0700

    Finished converting the EOFs analysis to a standalone Python script, and also broke out another Jupyter notebook to toy with it and do some more exploratory analysis before making final decisions.

--- phen/analysis/div/calc_LSP_EOFs.py
+++ phen/analysis/div/calc_LSP_EOFs.py
@@ -3,6 +3,7 @@
 # calc_LSP_EOFs.py
 
 import numpy as np
+import pandas as pd
 import geopandas as gpd
 from rasterio.plot import reshape_as_raster
 import xarray as xr
@@ -19,11 +20,12 @@ import phen_helper_fns as phf
 
 
 
-# TODO:
-  # could use clustering afterward, with scree plot, to determine K
-  # distinct 'classes' of global phenological seasonality 'types'
+###################
+# BEHAVIORAL PARAMS
+###################
 
-
+# save results?
+save_res = True
 
 # dataset params
 dataset = 'NIRv'
@@ -40,26 +42,6 @@ subplots_adj_top=0.9
 subplots_adj_wspace=0.2
 subplots_adj_hspace=0.4
 
-
-# NOTE on memory requirements:
-# I estimate that the global 365 x 2700 x 6900 raster will take up
-# about 54.4 GB memory; (I could also cast of dtype float32 and cut down
-# to ~27 GB). If I run this on savio bigmem that would only be ~1/9th total RAM,
-# so it should be doable to create a giant global 365 x 2700 x 6900
-# numpy array and then fill it with all fitted time series and write
-# to disk, though that's only ~1/2 RAM on savio3, so first I'll just try there!
-# Then I could separately read in as a dask array and run EOF
-# analysis on that, I believe.
-# Though I wonder if I could just run the EOF analysis within that
-# same job. Not sure how much EOF memory requirement expands as factor
-# of size of base spatiotemporal dataset, and not easily finding that
-# informiation on line...
-
-
-###################
-# BEHAVIORAL PARAMS
-###################
-
 # min and max x and y values, to optionally subset analysis to a region
 region_name = 'global'
 min_x = None
@@ -67,11 +49,6 @@ max_x = None
 min_y = None
 max_y = None
 
-# use hvplot, etc for interactive plotting?
-interactive = False
-
-# save figures and datasets?
-save_res = True
 
 # data dir on laptop
 if os.getcwd().split('/')[1] == 'home':
@@ -102,9 +79,6 @@ normalize_ts = True
 #lat_weights = 'cos'
 lat_weights = 'sqrt_cos'
 
-# center the data being input to EOF?
-center_eof = True
-
 # number of top EOFs to use?
 neofs = 4
 
@@ -114,9 +88,9 @@ ts_bot_pctile = 95
 n_ts_to_plot = 1000
 
 
-########################################
-# READ IN COEFFS, GET ARRAY OF FITTED TS
-########################################
+#################################
+# GET ARRAY OF FITTED TIME SERIES
+#################################
 
 # read global NIRv coeffs
 coeffs = rxr.open_rasterio(os.path.join(data_dir, '%s%s_coeffs.tif' % (dataset, mask_filename_ext)))
@@ -129,7 +103,7 @@ coeffs = coeffs.sel(x=slice(min_x, max_x), y=slice(max_y, min_y))
 X, Y = np.meshgrid(coeffs.x, coeffs.y)
 
 # filepath for the 365 x X x Y array of fitted time series
-ts_arr_filepath = os.path.join(data_dir, f'fitted_ts_array_{dataset}{mask_filename_ext}.txt')
+ts_arr_filepath = os.path.join(data_dir, f'fitted_ts_array_{dataset}{mask_filename_ext}.npy')
 
 # if that file doesn't exist then create it
 if not os.path.isfile(ts_arr_filepath):
@@ -158,21 +132,19 @@ if not os.path.isfile(ts_arr_filepath):
         assert ts.shape == (365,)
         ts_arr[:, i, j] = ts
         if n%10000 == 0:
-            print(f"\n\n\t{np.round(100*((n+1)/len(I)), 1)}% complete'")
+            print(f"\n\n\t{np.round(100*((n+1)/len(I)), 1)}% complete")
     # once complete, save this to a simple numpy array file
     # (for now, anyhow; might be worth saving as a big geospatial file eventually?)
     if save_res:
-        print('\n\n\tNOW SAVING TIME SERIES CUBE...\n\n(this will take some time...)')
-        np.savetxt(ts_arr_filepath, ts_arr.reshape(ts_arr.shape[0], -1))
+        print('\n\n\tNOW SAVING TIME SERIES CUBE...')
+        np.save(ts_arr_filepath, ts_arr)
 # otherwise read it in
 else:
     # load saved data and reshape it from 2d to 3d
-    ts_arr_2d = np.loadtxt(ts_arr_filepath)
-    ts_arr = ts_arr_2d.reshape(ts_arr_2d.shape[0],
-                               ts_arr_2d.shape[1] // coeffs.shape[2],
-                               coeffs.shape[2])
+    ts_arr = np.load(ts_arr_filepath)
+    assert np.all(ts_arr.shape == (365, coeffs.shape[1], coeffs.shape[2]))
 
-
+    
 #########
 # RUN EOF
 #########
@@ -208,14 +180,12 @@ ts_da.loc[:,:,:] = ts_arr
 solver = Eof(ts_da, weights=weights)
 
 # grab the first n EOFs
-# (and swap axes so that 3rd axis is of length neofs, to facilitate image
-# plotting)
 eofs = solver.eofsAsCorrelation(neofs=neofs)
 
 # grab the PCs
 pcs = solver.pcs(npcs=neofs, pcscaling=1)
 
-# grab pct variances of EOFs
+# grab percent variances of EOFs
 var_pcts = solver.varianceFraction(neofs)
 
 # reconstruct the field using just the selected top EOFs
@@ -239,73 +209,18 @@ if save_res:
                              str(var_pcts.values[i]) for i in range(neofs)},
                       )
 
-fig = plt.figure(figsize=(16,8))
-ax = fig.add_subplot(111)
-eofs.sel(mode=1).plot.imshow(ax=ax)
+# save the PC values and percents variance explained to a CSV, for use in supplemental figure
+pc_dict = {f"EOF{neof}_{float(np.round(var_pcts[neof]*100, 2))}pct": pcs.sel(mode=neof).values for neof in range(4)}
+pc_df = pd.DataFrame.from_dict(pc_dict)
+pc_df_filepath = os.path.join(data_dir, f'{dataset}{mask_filename_ext}_EOF_PC_table.csv')
+pc_df.to_csv(pc_df_filepath, index=False)
 
-fig, ax = plt.subplots(1,1)
+# save a simple scree plot, for later reference 
+scree_fig = plt.figure(figsize=(4,4))
+ax = scree_fig.add_subplot(111)
 ax.plot(solver.varianceFraction(10)*100)
 ax.set_title('pct variance explained by first 10 EOFs')
 ax.set_xlabel('EOF number')
 ax.set_ylabel('pct variance explained')
-print([*solver.varianceFraction(10).values*100])
-
-
-#########################################################################
-# MAP AND PLOT CHARACTERISTIC SEASONAL PATTERNS AT ENDS OF EACH EOF RANGE
-#########################################################################
-
-fig = plt.figure(figsize=(12,9))
-gs = fig.add_gridspec(nrows=neofs, ncols=5, width_ratios=[1,0.3,1,0.3,1])
-for neof in range(neofs):
-    ax_map = fig.add_subplot(gs[neof, 0])
-    eofs.sel(mode=neof).plot.imshow(ax=ax_map)
-    ax_map.set_aspect('equal')
-    ax_map.set_xticks(())
-    ax_map.set_yticks(())
-    ax_map.set_xticklabels(())
-    ax_map.set_yticklabels(())
-    ax_map.set_ylabel('mode %i' % neof, fontdict={'fontsize': 20})
-    ax_map.set_title('%0.1f%% of variation' % (var_pcts[neof]*100))
-    ax_pc_plot = fig.add_subplot(gs[neof,2])
-    ax_pc_plot.set_xlabel('time of year')
-    ax_pc_plot.set_xticks(np.linspace(0, 365, 5), ['Jan', 'Apr', 'Jul',
-                                                   'Oct', 'Jan'])
-    pcs.sel(mode=neof).plot(ax=ax_pc_plot)
-    ax_pc_plot.set_title('annual variation')
-    ax_pc_plot.set_ylabel('scaled PC value')
-    ax_examp_ts = fig.add_subplot(gs[neof,4])
-    top_ts = ts_recon.values[:,
-        eofs.sel(mode=neof)>=np.nanpercentile(eofs.sel(mode=neof), ts_top_pctile)].T
-    idxs_to_plot = np.random.choice(range(top_ts.shape[0]), size=n_ts_to_plot, replace=False)
-    ts_to_plot = top_ts[idxs_to_plot,:]
-    for ts in ts_to_plot:
-        ax_examp_ts.plot(range(365), ts, alpha=0.05, color='#850c10')
-    bot_ts = ts_recon.values[:,
-        eofs.sel(mode=neof)<=np.nanpercentile(eofs.sel(mode=neof), ts_bot_pctile)].T
-    idxs_to_plot = np.random.choice(range(bot_ts.shape[0]), size=n_ts_to_plot, replace=False)
-    ts_to_plot = bot_ts[idxs_to_plot,:]
-    for ts in ts_to_plot:
-        ax_examp_ts.plot(range(365), ts, alpha=0.05, color='#0b489e')
-    ax_examp_ts.set_xlabel('time of year')
-    ax_examp_ts.set_ylabel('scaled magnitude')
-    ax_examp_ts.set_title(('seasonality at 95th pctile (red)\n'
-                        'and 5th pctile (blue) of mode %i pixels') % neof)
-    ax_examp_ts.set_xticks(np.linspace(0, 365, 5), ['Jan', 'Apr', 'Jul',
-                                                    'Oct', 'Jan'])
-fig.subplots_adjust(left=subplots_adj_left,
-                     bottom=subplots_adj_bottom,
-                     right=subplots_adj_right,
-                     top=subplots_adj_top,
-                     wspace=subplots_adj_wspace,
-                     hspace=subplots_adj_hspace)
-fig.show()
-if save_res:
-    fig_filename = '%s_%s_EOF_results_scat_%s%s%s.png' % (dataset,
-                                                            region_name,
-                                                            lat_weights + 'wts',
-                                                            '_normts' * normalize_ts,
-                                                            mask_filename_ext,
-                                                           )
-    fig.savefig(os.path.join(data_dir, fig_filename), dpi=500)
-
+scree_fig_filepath = os.path.join(data_dir, f'{dataset}{mask_filename_ext}_scree_fig.png')
+scree_fig.save_fig(scree_fig_filepath, dpi=500)
