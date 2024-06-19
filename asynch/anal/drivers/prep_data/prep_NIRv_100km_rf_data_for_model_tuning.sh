a19f9174e9ac761af04b9a0e6271d817cdc9c5dc
Drew Hart <drew.ellison.hart@gmail.com>
2024-06-19 03:55:47 -0700

    Fix minor bug in the 100km NIRv rf data-prep bash script

--- asynch/anal/drivers/prep_data/prep_NIRv_100km_rf_data_for_model_tuning.sh
+++ asynch/anal/drivers/prep_data/prep_NIRv_100km_rf_data_for_model_tuning.sh
@@ -1,2 +1,2 @@
 module load gdal/2.2.3 r/4.0.3 r-spatial/2020-11-30-r40
-Rscript --vanilla ~/seasonality/seasonal_asynchrony/asynch/anal/drivers/prep_data/prep_asynch_rf_data.r NIRv 100
+Rscript --vanilla ~/seasonality/seasonal_asynchrony/asynch/anal/drivers/prep_data/prep_phen_asynch_rf_data.r NIRv 100
