383721cada6c1b85c27b901b9c01c283a75c5111
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2024-06-19 22:52:48 +1000

    Random forest job failed to load sf and rgdal as soon as I launched it with slurm. Trying adding Chris Paciorek's fixer from the first 2 lines of the rf-running R script to the top of the data-prep R script too.

--- asynch/anal/drivers/prep_data/cp_asynch_files_to_rf_data.sh
+++ asynch/anal/drivers/prep_data/cp_asynch_files_to_rf_data.sh
@@ -1,3 +1,5 @@
-for dir in `echo NIRv NIRv_STRICT SIF SIF_STRICT tmmn tmmx pr def cloud`
+# NOTE: don't copy the non-strict-masked NIRv or SIF files, to avoid any risk
+#       of accidentally using them as the random forest response variables
+for dir in `echo NIRv_STRICT SIF_STRICT tmmn tmmx pr def cloud`
   do cp /global/scratch/users/drewhart/seasonality/GEE_outputs/$dir/*asynch*.tif /global/scratch/users/drewhart/seasonality/rf_data/
   done
