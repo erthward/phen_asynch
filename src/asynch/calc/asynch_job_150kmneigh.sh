bdfcdc37e70c591515e95daea8b7ee4536821959
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2024-07-26 21:59:02 +1000

    Fixed broken paths in the 50km and 150km asynch job scripts

--- src/asynch/calc/asynch_job_150kmneigh.sh
+++ src/asynch/calc/asynch_job_150kmneigh.sh
@@ -27,7 +27,7 @@ do
       echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
       echo "NOW PROCESSING ${var} FOR NEIGH RAD ${neigh_rad} METERS..."
       # NOTE: call Julia v1.4.1, now removed from `module avail`
-      /global/home/groups/consultsw/sl-7.x86_64/modules/julia/1.4.1/bin/julia -p 32 /global/home/users/drewhart/seasonality/seasonal_asynchrony/asynch/src/calc/calc_asynch.jl --neigh_rad $neigh_rad --var $var > ch3_asynch_job_${var}_${neigh_rad}.jlout
+      /global/home/groups/consultsw/sl-7.x86_64/modules/julia/1.4.1/bin/julia -p 32 /global/home/users/drewhart/seasonality/seasonal_asynchrony/src/asynch/calc/calc_asynch.jl --neigh_rad $neigh_rad --var $var > ch3_asynch_job_${var}_${neigh_rad}.jlout
       echo "FINISHED PROCESSING ${var} FOR NEIGH RAD ${neigh_rad} METERS."
    done
 done
